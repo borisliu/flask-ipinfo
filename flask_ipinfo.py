3481fd2b676e67c864c6deab8a351104a0aa168a
borisliu <liulijun@263.net>
2017-01-02 21:39:45 +0800

    Upload the 0.0.4 version

--- flask_ipinfo.py
+++ flask_ipinfo.py
@@ -1,34 +1,14 @@
 import re
 import urllib
 import json
-from flask import current_app
+from flask import request
 
-# Find the stack on which we want to store the database connection.
-# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
-# before that we need to use the _request_ctx_stack.
-try:
-    from flask import _app_ctx_stack as stack
-except ImportError:
-    from flask import _request_ctx_stack as stack
+__version__  = '0.0.4'
+__author__ = "borisliu"
 
 
 class IPInfo(object):
-
-    def __init__(self, app=None):
-        self.app = app
-        if app is not None:
-            self.init_app(app)
-
-    def init_app(self, app):
-        # Use the newstyle teardown_appcontext if it's available,
-        # otherwise fall back to the request context
-        if hasattr(app, 'teardown_appcontext'):
-            app.teardown_appcontext(self.teardown)
-        else:
-            app.teardown_request(self.teardown)
-
-    def teardown(self, exception):
-        ctx = stack.top
+    """IP地址类."""
 
     @property
     def browser(self):
@@ -36,7 +16,13 @@ class IPInfo(object):
         ua = request.headers.get('User-Agent')
         if (ua):
             browser = ua
-            if (re.compile(r'MSIE', re.I).search(ua) or re.compile(r'rv:([^\)]+)\) like Gecko', re.I).search(ua)):
+            if (re.compile(r'MicroMessenger', re.I).search(ua)):
+                browser = 'WeChat'
+            elif (re.compile(r'MinxingMessenger', re.I).search(ua)):
+                browser = 'MinxingMessenger'
+            elif (re.compile(r'QQ', re.I).search(ua)):
+                browser = 'TencentQQ'
+            elif (re.compile(r'MSIE', re.I).search(ua) or re.compile(r'rv:([^\)]+)\) like Gecko', re.I).search(ua)):
                 browser = 'MSIE'
             elif (re.compile(r'Firefox', re.I).search(ua)):
                 browser = 'Firefox'
@@ -46,12 +32,6 @@ class IPInfo(object):
                 browser = 'Safari'
             elif (re.compile(r'Opera', re.I).search(ua)):
                 browser = 'Opera'
-            elif (re.compile(r'MinxingMessenger', re.I).search(ua)):
-                browser = 'MinxingMessenger'
-            elif (re.compile(r'QQ', re.I).search(ua)):
-                browser = 'TencentQQ'
-            elif (re.compile(r'MicroMessenger', re.I).search(ua)):
-                browser = 'WeChat'
             return(browser)
         else:
             return('Unknown')
@@ -99,44 +79,8 @@ class IPInfo(object):
     @property
     def ipaddress(self):
         """获得IP地址."""
-        if (request.headers.get('Client-IP')):
-            ip = request.headers.get('Client-IP')
-        if (request.headers.get('X-Forwarded-For')):  # 获取代理ip
-            ips = request.headers.get('X-Forwarded-For').split(',')
-            if (ip):
-                ips.insert(0, ip)
-            for i in ips:
-                if (not re.compile('^(10|172\.16|192\.168)\.', re.I).match(i) and i != 'unknown'):  # 排除局域网ip
-                    ip = i
-                    break
-        tip = request.remote_addr if (request.remote_addr) else ip
-        if (tip == '127.0.0.1'):  # 获得本地真实IP
-            return self.getOnlineIP()
+        ip = request.remote_addr
+        if (ip):
+            return(ip)
         else:
-            return(tip)
-
-    def getOnlineIP(self):
-        """获得本地真实IP."""
-        before = time.time()
-        html = urllib.request.urlopen(
-            'http://ip.taobao.com/service/getIpInfo.php?ip=myip').read()
-        after = time.time()
-        self.taobao_timeout = self.taobao_timeout + int(after - before)
-        ipArray = json.loads(str(html, encoding='utf-8'))
-        if (ipArray and ipArray['code'] == 0):
-            return ipArray['data']['ip']
-
-    @property
-    def location(self, request, ip):
-        """根据ip获得访客所在地地名."""
-        if (not ip):
-            ip = self.ipaddress()
-
-        html = urllib.request.urlopen(
-            'http://ip.taobao.com/service/getIpInfo.php?ip=%s' % ip).read()
-        ipArray = json.loads(str(html, encoding='utf-8'))
-
-        if (ipArray and ipArray['code'] == 0):
-            return ipArray
-        else:
-            return False
+            return('Unknown')
--- setup.py
+++ setup.py
@@ -5,35 +5,46 @@ Flask-IPInfo
 This library get some useful information from flask's request object.Such as 
 IP Address, ISP, Location, Web Browser, Operating System, etc.
 """
+import os
+import sys
+import flask_ipinfo
+
+if sys.argv[-1] == 'publish':
+    os.system("del /Q dist\\*")
+    os.system("python setup.py bdist_wheel")
+    os.system("twine upload dist/*")
+    sys.exit()
+
 from setuptools import setup
 
+#Get version
+version = flask_ipinfo.__version__
+
+#Get long description.
+long_description = ""
+with open("README.rst") as f:
+    long_description = f.read()
 
 setup(
     name='Flask-IPInfo',
-    version='1.0',
+    version=version,
     url='http://github.com/borisliu/flask-ipinfo/',
     license='MIT',
-    author='boris liu',
+    author='borisliu',
     author_email='boris_cn@263.net',
     description='Get IP, ISP, Location, OS from flask request',
-    long_description=__doc__,
+    long_description=long_description,
+    platforms=['all'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
     py_modules=['flask_ipinfo'],
-    # if you would be using a package instead use packages instead
-    # of py_modules:
-    # packages=['flask_sqlite3'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
     install_requires=[
         'Flask'
     ],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
 )
